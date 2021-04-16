e06da3c500e69665e84c8e3398ea654b10e85a12
sunchao <1306519243@qq.com>
2021-04-16 14:11:22 +0800

    增加告警日志和操作日志

--- src/api/monitor/OperLog.js
+++ src/api/monitor/OperLog.js
@@ -4,6 +4,7 @@ import qs from 'qs'
 
 /**
  * 查询分页数据
+ * 暂时没用到（已被方法queryOperLogPageByTime替代）
  */
 export const queryOperLogPageList = info => {
     return axios.request({
@@ -11,6 +12,17 @@ export const queryOperLogPageList = info => {
         method: 'post',
         data: qs.stringify(info)
     })
+}
+
+/**
+ * 按照检索条件分页查询
+ */
+export const queryOperLogPageByTime = info => {
+  return axios.request({
+    url: xy.Setting.SPACE_URL + '/monitor/operLog/queryOperLog',
+    method: 'post',
+    data: qs.stringify(info)
+  })
 }
 
 /**
