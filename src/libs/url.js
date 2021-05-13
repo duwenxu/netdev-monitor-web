ce53f55f52de961a2f0512bd60baff64dfe49795
zx <zxxzoooo@163.com>
2021-05-13 15:04:35 +0800

    暂存模拟数据

--- src/libs/url.js
+++ src/libs/url.js
@@ -10,8 +10,8 @@ let proto = document.location.protocol
 // Setting.SPACE_URL = '/track'
 // Setting.SOCKET_URL = '/track_socket'
 
-Setting.SPACE_URL = ('https:' == proto ? '' : 'http://localhost:8080')
-Setting.SOCKET_URL = ('https:' == proto ? '' : 'localhost:8888')
+Setting.SPACE_URL = ('https:' == proto ? '' : 'http://172.21.5.226:8080')
+Setting.SOCKET_URL = ('https:' == proto ? '' : '172.21.5.226:8888')
 //
 // Setting.SPACE_URL = ('https:' == proto ? '' : 'http://172.21.7.4:8080')
 // Setting.SOCKET_URL = ('https:' == proto ? '' : '172.21.7.4:8888')
